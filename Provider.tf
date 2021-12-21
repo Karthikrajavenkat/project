34fa3952d2a0f64b1561f5e12216a1dd6f9ef34e
Karthikrajavenkat <96491755+Karthikrajavenkat@users.noreply.github.com>
2021-12-21 12:01:01 -0600

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = "${var.region}"
-  access_key = "AKIA4HXEVKWPQ5SJ5FVM"
-    secret_key = "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"
+  access_key = "Access key"
+    secret_key = "Secret key"
 }
 
 
